b46587b5f632e86a732f2746e260b0ef112dd992
Vytenis Bivainis <monrealis@gmail.com>
2015-02-23 00:44:28 +0200

    Use beforeEach.

--- tests/testWithAssert.js
+++ tests/testWithAssert.js
@@ -2,11 +2,17 @@ var assert = require("assert");
 require("should");
 
 describe('Array#indexOf', function () {
+    var array;
+
+    beforeEach(function () {
+        array = [1, 2, 3];
+    });
+
     it('should return -1 when the value is not present', function () {
-        assert.equal(-1, [1, 2, 3].indexOf(4));
+        assert.equal(-1, array.indexOf(4));
     });
 
     it('should return nonnegative value if value is present', function () {
-        [1, 2, 3].indexOf(2).should.equal(1);
+        array.indexOf(2).should.equal(1);
     });
 });
