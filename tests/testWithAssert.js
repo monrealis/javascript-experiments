7a49ee94e6e974f825d02cf45b5225848a9aa4aa
Vytenis Bivainis <monrealis@gmail.com>
2015-02-23 00:24:31 +0200

    Add should assertions.

--- tests/testWithAssert.js
+++ tests/testWithAssert.js
@@ -1,7 +1,12 @@
 var assert = require("assert");
+require("should");
 
 describe('Array#indexOf', function () {
     it('should return -1 when the value is not present', function () {
         assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+
+    it('should return nonnegative value if value is present', function () {
+        [1, 2, 3].indexOf(2).should.equal(1);
     });
 });
