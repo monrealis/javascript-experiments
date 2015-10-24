9acf72aceb6c1284103e3041444e575450060b21
Vytenis Bivainis <monrealis@gmail.com>
2015-10-24 21:47:58 +0300

    Connect two scopes using same Object from factory.

--- angular/main.js
+++ angular/main.js
@@ -1,11 +1,14 @@
-var app = angular.module('MyApp', [])
-app.controller('Controller1', Controller1)
-app.controller('Controller2', Controller2)
+var app = angular.module('MyApp', []);
+app.controller('Controller1', Controller1);
+app.controller('Controller2', Controller2);
+app.factory('Data', function () {
+    return {message: 'Hello world'}
+});
 
-function Controller1($scope) {
-    // $scope.data = {message: "Hello World"};
+function Controller1($scope, Data) {
+    $scope.data = Data;
 }
 
-function Controller2($scope) {
-    // $scope.data = {message: "Hello World"};
+function Controller2($scope, Data) {
+    $scope.data = Data;
 }
