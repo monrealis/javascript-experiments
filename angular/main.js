346c10f4462c86315d8c00776a4b98d3272328af
Vytenis Bivainis <monrealis@gmail.com>
2015-10-24 21:31:58 +0300

    Use two controllers.

--- angular/main.js
+++ angular/main.js
@@ -1,4 +1,11 @@
-angular.module('HelloWorldApp', [])
-    .controller('HelloWorldController', function($scope) {
-        $scope.greeting = "Hello World";
-    });
+var app = angular.module('MyApp', [])
+app.controller('Controller1', Controller1)
+app.controller('Controller2', Controller2)
+
+function Controller1($scope) {
+    $scope.data = {message: "Hello World"};
+}
+
+function Controller2($scope) {
+    $scope.data = {message: "Hello World"};
+}
