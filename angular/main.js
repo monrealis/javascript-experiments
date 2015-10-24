a5a13a691e89427ea3d97c5235e44454a426387b
Vytenis Bivainis <monrealis@gmail.com>
2015-10-24 21:35:18 +0300

    Use data.

--- angular/main.js
+++ angular/main.js
@@ -3,9 +3,9 @@ app.controller('Controller1', Controller1)
 app.controller('Controller2', Controller2)
 
 function Controller1($scope) {
-    $scope.data = {message: "Hello World"};
+    // $scope.data = {message: "Hello World"};
 }
 
 function Controller2($scope) {
-    $scope.data = {message: "Hello World"};
+    // $scope.data = {message: "Hello World"};
 }
