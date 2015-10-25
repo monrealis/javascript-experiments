27a16df6fc5fc168e1ad0707cd9d42fd18d9054d
Vytenis Bivainis <monrealis@gmail.com>
2015-10-25 13:34:01 +0200

    Add filter.html.

---
+++ angular/filter.js
@@ -0,0 +1,11 @@
+var app = angular.module('myApp', []);
+app.controller('Controller1', Controller1);
+app.factory('Names', createNames);
+
+function Controller1($scope, Names) {
+    $scope.names = Names;
+}
+
+function createNames() {
+    return [{first: 'First', last: 'Last'}, {first: 'Second', last: 'Last2'}];
+}
