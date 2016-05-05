5b76a501649094bb638dcfbb155d5bd0e32281f1
Vytenis Bivainis <monrealis@gmail.com>
2016-05-05 22:42:10 +0300

    Generate unique numbers.

--- tests/takerTest.js
+++ tests/takerTest.js
@@ -5,15 +5,23 @@ var Taker = function (maxNumber) {
 };
 
 Taker.prototype.take = function (n) {
+    function nextRandom(alreadyUsed) {
+        var randomInt = Math.floor(Math.random() * takerThis.maxNumber);
+        if (alreadyUsed.indexOf(randomInt) >= 0)
+            return nextRandom(alreadyUsed);
+        return randomInt;
+    }
+
+    var takerThis = this;
     var r = [];
     for (var i = 0; i < n; ++i)
-        r.push(Math.floor(Math.random() * 60));
+        r.push(nextRandom(r));
     return r;
 };
 
 describe("taker", function () {
     beforeEach(function () {
-        this.taker = new Taker();
+        this.taker = new Taker(60);
     });
 
     it("should take 10 numbers within a range", function () {
@@ -22,12 +30,12 @@ describe("taker", function () {
 
     it("should take unique numbers", function () {
         var hash = [];
-        this.taker.take(30).forEach(function (n) {
+        this.taker.take(10).forEach(function (n) {
             if (hash[n] == null)
                 hash[n] = 0;
             hash[n]++;
         })
-        chai.expect(Object.keys(hash).length).to.equal(30)
+        chai.expect(Object.keys(hash).length).to.equal(10)
     })
 
     function takeTen() {
