7cff0b4c3ec7aa0e6b8f5ad83074dcd2ac4e0234
Vytenis Bivainis <monrealis@gmail.com>
2016-05-05 22:13:58 +0300

    Refactor Taker API.

--- tests/takerTest.js
+++ tests/takerTest.js
@@ -1,33 +1,36 @@
 var chai = require("chai");
 
-var Taker = function (n) {
-    this.n = n
+var Taker = function (maxNumber) {
+    this.maxNumber = maxNumber
 };
 
-Taker.prototype.take = function () {
+Taker.prototype.take = function (n) {
     var r = [];
-    for (var i = 0; i < this.n; ++i)
+    for (var i = 0; i < n; ++i)
         r.push(i);
     return r;
 };
 
 describe("taker", function () {
     beforeEach(function () {
-        this.taker = new Taker(10);
-        this.take = this.taker.take;
+        this.taker = new Taker();
     });
 
     it("should take 10 numbers within a range", function () {
-        chai.expect(this.taker.take().length).to.equal(10);
+        chai.expect(this.taker.take(10).length).to.equal(10);
     });
 
     it("should take unique numbers", function () {
         var hash = [];
-        this.taker.take().forEach(function (n) {
+        this.taker.take(10).forEach(function (n) {
             if (hash[n] == null)
                 hash[n] = 0;
             hash[n]++;
         })
         chai.expect(hash.length).to.equal(10)
     })
+
+    function takeTen() {
+        return this.taker.take(10)
+    }
 });
