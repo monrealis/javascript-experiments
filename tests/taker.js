8be4e894ff8ebe89f08b20bc76c55c0652bc6a2e
Vytenis Bivainis <monrealis@gmail.com>
2016-05-13 00:52:29 +0300

    Move taker to it's own file.

---
+++ tests/taker.js
@@ -0,0 +1,20 @@
+var Taker = function (maxNumber) {
+    this.maxNumber = maxNumber
+};
+
+Taker.prototype.take = function (n) {
+    function nextRandom(alreadyUsed) {
+        var randomInt = Math.floor(Math.random() * takerThis.maxNumber);
+        if (alreadyUsed.indexOf(randomInt) >= 0)
+            return nextRandom(alreadyUsed);
+        return randomInt;
+    }
+
+    var takerThis = this;
+    var r = [];
+    for (var i = 0; i < n; ++i)
+        r.push(nextRandom(r));
+    return r;
+};
+
+module.exports = Taker;
--- tests/takerTest.js
+++ tests/takerTest.js
@@ -1,25 +1,9 @@
 var chai;
-if (typeof require != 'undefined')
+var Taker;
+if (typeof require != 'undefined') {
     chai = require("chai");
-
-var Taker = function (maxNumber) {
-    this.maxNumber = maxNumber
-};
-
-Taker.prototype.take = function (n) {
-    function nextRandom(alreadyUsed) {
-        var randomInt = Math.floor(Math.random() * takerThis.maxNumber);
-        if (alreadyUsed.indexOf(randomInt) >= 0)
-            return nextRandom(alreadyUsed);
-        return randomInt;
-    }
-
-    var takerThis = this;
-    var r = [];
-    for (var i = 0; i < n; ++i)
-        r.push(nextRandom(r));
-    return r;
-};
+    Taker = require("./taker")
+}
 
 describe("taker", function () {
     var takerThis;
