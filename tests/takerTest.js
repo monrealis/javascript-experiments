43e84b9ded55193a486cef1d5a2e7ac557dc1334
Vytenis Bivainis <monrealis@gmail.com>
2016-05-13 00:57:07 +0300

    Move game simulator to it's own file.

--- tests/takerTest.js
+++ tests/takerTest.js
@@ -1,8 +1,10 @@
 var chai;
 var Taker;
+var GameSimulator;
 if (typeof require != 'undefined') {
     chai = require("chai");
     Taker = require("./taker")
+    GameSimulator = require("./gameSimulator")
 }
 
 describe("taker", function () {
@@ -39,33 +41,6 @@ describe("taker", function () {
         })
     })
 });
-
-var GameSimulator = function (numberOfGames) {
-    this.taker = new Taker(60);
-    this.guesser = this.taker;
-    this.roller = this.taker;
-    this.numberOfGames = numberOfGames;
-};
-
-GameSimulator.prototype.getNumberOfWins = function () {
-    var r = 0;
-    for (var i = 0; i < this.numberOfGames; ++i)
-        if (this.getNumberOfShared() == 2)
-            r++;
-    return r;
-};
-
-GameSimulator.prototype.getNumberOfShared = function () {
-    var guesses = this.guesser.take(2);
-    var actual = this.roller.take(20);
-    var r = 0;
-    guesses.forEach(function (guess) {
-        if (actual.indexOf(guess) >= 0)
-            ++r;
-    });
-    return r;
-};
-
 
 describe("game simulator", function () {
     beforeEach(function () {
